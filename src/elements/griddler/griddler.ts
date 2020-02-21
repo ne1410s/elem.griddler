1aac2115871046d4d9f0e1fc72ec9898ed6d9693
ne1410s <pgj646@gmail.com>
2020-02-21 22:41:57 +0000

    Support for higher resolution

--- src/elements/griddler/griddler.ts
+++ src/elements/griddler/griddler.ts
@@ -1,7 +1,6 @@
 import { CustomElementBase } from '@ne1410s/cust-elems';
 import markupUrl from './griddler.html';
 import stylesUrl from './griddler.css';
-import { DomCommsHelper } from '../dom-comms-helper';
 
 export class Griddler extends CustomElementBase {
 
@@ -12,16 +11,22 @@ export class Griddler extends CustomElementBase {
   private static readonly XY_INTERVAL = 5;
   private static readonly DEF_X = 25;
   private static readonly DEF_Y = 35;
-  private static readonly SIZE_MIN = 5;
-  private static readonly SIZE_MAX = 50;
-  private static readonly SIZE_INTERVAL = 1;
-  private static readonly DEF_SIZE = 20;
+  private static readonly MINOR_COL = '#eee';
+  private static readonly MAJOR_COL = '#555';
   private static readonly HIGHLIGHT = 'rgba(0, 0, 200, 0.4)';
+  private static readonly RESOLUTION = 10;
 
+  private static readonly SIZE_MIN = 5 * Griddler.RESOLUTION;
+  private static readonly SIZE_MAX = 50 * Griddler.RESOLUTION;
+  private static readonly SIZE_INTERVAL = 1 * Griddler.RESOLUTION;
+  private static readonly DEF_SIZE = 20 * Griddler.RESOLUTION;
+  private static readonly PIXEL_ADJUST = .5 * Griddler.RESOLUTION;
+  private static readonly LABEL_ROOM = 5 * Griddler.RESOLUTION;
+  
   private readonly _gridCanvas: HTMLCanvasElement;
   private readonly _hiliteCanvas: HTMLCanvasElement;
   private readonly _hiliteContext: CanvasRenderingContext2D;
-
+  
   private _cols = Griddler.DEF_X;
   private _rows = Griddler.DEF_Y;
   private _size = Griddler.DEF_SIZE;
@@ -44,10 +49,7 @@ export class Griddler extends CustomElementBase {
     this._gridCanvas = this.root.querySelector('canvas#grid');
     this._hiliteCanvas = this.root.querySelector('canvas#hilite');
     this._hiliteContext = this._hiliteCanvas.getContext('2d');
-
-    // this._gridCanvas.addEventListener('mousemove', DomCommsHelper.Throttle((event: MouseEvent) => {
-    //   this.onMouseMove(event);
-    // }, 20));
+    this._hiliteContext.imageSmoothingEnabled = false;
 
     this._gridCanvas.addEventListener('mouseleave', () => this.clear(this._hiliteContext));
     this._gridCanvas.addEventListener('mousemove', (e: MouseEvent) => this.onMouseMove(e));
@@ -58,40 +60,44 @@ export class Griddler extends CustomElementBase {
    * Draws a new grid according to the current size configuration.
    */
   redraw(): void {
-    const labels_w = this._cols * 5;
-    const labels_h = this._rows * 5;
-    const grid_w = this._cols * this._size + .5;
-    const grid_h = this._rows * this._size + .5;
+    const labels_w = this._cols * Griddler.LABEL_ROOM;
+    const labels_h = this._rows * Griddler.LABEL_ROOM;
+    const grid_w = this._cols * this._size + Griddler.PIXEL_ADJUST;
+    const grid_h = this._rows * this._size + Griddler.PIXEL_ADJUST;
 
     this.totalWidth = grid_w + labels_w;
     this.totalHeight = grid_h + labels_h;
+    const client_w = this.totalWidth / Griddler.RESOLUTION;
+    this.root.querySelector('.root').setAttribute('style', `max-width: ${client_w}px`);
 
     const gridContext = this._gridCanvas.getContext('2d');
     this.clear(gridContext);
-
+    gridContext.imageSmoothingEnabled = false;
     gridContext.beginPath();
     for (let c = 0; c <= this._cols; c++) {
-      gridContext.moveTo(c * this._size + .5, 0);
-      gridContext.lineTo(c * this._size + .5, grid_h);
+      gridContext.moveTo(c * this._size + Griddler.PIXEL_ADJUST, 0);
+      gridContext.lineTo(c * this._size + Griddler.PIXEL_ADJUST, grid_h);
     }
     for (let r = 0; r <= this._rows; r++) {
-      gridContext.moveTo(0, r * this._size + .5);
-      gridContext.lineTo(grid_w, r * this._size + .5);
+      gridContext.moveTo(0, r * this._size + Griddler.PIXEL_ADJUST);
+      gridContext.lineTo(grid_w, r * this._size + Griddler.PIXEL_ADJUST);
     }
-    gridContext.strokeStyle = '#ccc';
+    gridContext.strokeStyle = Griddler.MINOR_COL;
+    gridContext.lineWidth = Griddler.RESOLUTION;
     gridContext.stroke();    
     gridContext.closePath();
 
     gridContext.beginPath();
     for (let ci = 0; ci <= this._cols; ci += Griddler.XY_INTERVAL) {
-      gridContext.moveTo(ci * this._size + .5, 0);
-      gridContext.lineTo(ci * this._size + .5, grid_h);
+      gridContext.moveTo(ci * this._size + Griddler.PIXEL_ADJUST, 0);
+      gridContext.lineTo(ci * this._size + Griddler.PIXEL_ADJUST, grid_h);
     }
     for (let ri = 0; ri <= this._rows; ri += Griddler.XY_INTERVAL) {
-      gridContext.moveTo(0, ri * this._size + .5);
-      gridContext.lineTo(grid_w, ri * this._size + .5);
+      gridContext.moveTo(0, ri * this._size + Griddler.PIXEL_ADJUST);
+      gridContext.lineTo(grid_w, ri * this._size + Griddler.PIXEL_ADJUST);
     }
-    gridContext.strokeStyle = '#000';
+    gridContext.strokeStyle = Griddler.MAJOR_COL;
+    gridContext.lineWidth = Griddler.RESOLUTION;
     gridContext.stroke();    
     gridContext.closePath();
   }
@@ -142,8 +148,8 @@ export class Griddler extends CustomElementBase {
 
   private getCoords(offsetX: number, offsetY: number): { x: number, y: number } {
     return {
-      x: Griddler.Round(offsetX / this._size, 0, 1, 0, this._cols),
-      y: Griddler.Round(offsetY / this._size, 0, 1, 0, this._rows),
+      x: Griddler.Round(offsetX * Griddler.RESOLUTION / this._size, 0, 1, 0, this._cols),
+      y: Griddler.Round(offsetY * Griddler.RESOLUTION / this._size, 0, 1, 0, this._rows),
     };
   }
 
@@ -153,10 +159,10 @@ export class Griddler extends CustomElementBase {
     this.clear(this._hiliteContext);
     
     if (coords.x !== this._cols) {
-      this._hiliteContext.fillRect(coords.x * this._size + 1.5, 0, this._size, this.totalHeight);
+      this._hiliteContext.fillRect(coords.x * this._size + Griddler.PIXEL_ADJUST, 0, this._size, this.totalHeight);
     }
     if (coords.y !== this._rows) {
-      this._hiliteContext.fillRect(0, coords.y * this._size + 1.5, this.totalWidth, this._size);
+      this._hiliteContext.fillRect(0, coords.y * this._size + Griddler.PIXEL_ADJUST, this.totalWidth, this._size);
     }
   }
 
