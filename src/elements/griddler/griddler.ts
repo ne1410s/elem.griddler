040509d30484cb9aa8a6e18740be0d804f1dd321
Paul Jones <pjjones@siemens.com>
2020-02-26 13:00:53 +0000

    Downloads

--- src/elements/griddler/griddler.ts
+++ src/elements/griddler/griddler.ts
@@ -17,7 +17,9 @@ export class Griddler extends CustomElementBase {
   private static readonly RESOLUTION = 2;
   private static readonly MINOR_COL = '#eee';
   private static readonly MAJOR_COL = '#555';
-  private static readonly HIGHLIGHT = 'rgba(0, 0, 200, 0.4)';
+  private static readonly LABEL_COL = '#000';
+  private static readonly CELL_COL = '#000';
+  private static readonly HILITE = 'rgba(0, 0, 200, 0.4)';
 
   private static readonly SIZE_MIN = 5 * Griddler.RESOLUTION;
   private static readonly SIZE_MAX = 50 * Griddler.RESOLUTION;
@@ -26,6 +28,7 @@ export class Griddler extends CustomElementBase {
   private static readonly PIXEL_ADJUST = .5 * Griddler.RESOLUTION;
   
   private readonly _gridCanvas: HTMLCanvasElement;
+  private readonly _gridContext: CanvasRenderingContext2D;
   private readonly _hiCanvas: HTMLCanvasElement;
   private readonly _hiContext: CanvasRenderingContext2D;
 
@@ -48,10 +51,25 @@ export class Griddler extends CustomElementBase {
     this._hiCanvas.height = value;
   }
 
+  public toString(): string {
+    return JSON.stringify(XGrid.ToDense(this._grid));
+  }
+
+  get textDataUrl(): string {
+    const encoded = window.encodeURIComponent(this.toString());
+    return `data:text/plain;charset=utf-8,${encoded}`;
+  }
+
+  get imageDataUrl(): string {
+    return this._gridCanvas.toDataURL();
+  }
+
   constructor() {
 
     super(stylesUrl, markupUrl);
     this._gridCanvas = this.root.querySelector('canvas#grid');
+    this._gridContext = this._gridCanvas.getContext('2d');
+    this._gridContext.imageSmoothingEnabled = false;
     this._hiCanvas = this.root.querySelector('canvas#hilite');
     this._hiContext = this._hiCanvas.getContext('2d');
     this._hiContext.imageSmoothingEnabled = false;
@@ -105,7 +123,9 @@ export class Griddler extends CustomElementBase {
 
     this.root.querySelector('#btnSolve').addEventListener('click', () => this.solve());
     this.root.querySelector('#btnClear').addEventListener('click', () => this.clear());
-    this.root.querySelector('#btnExport').addEventListener('click', () => this.export());
+    this.root.querySelector('#btnExport').addEventListener('click', () => Griddler.Download(this.textDataUrl, 'My Grid.json'));
+    this.root.querySelector('#btnDownload').addEventListener('click', () => Griddler.Download(this.imageDataUrl, 'My Grid.png'));
+    this.root.querySelector('#btnPrint').addEventListener('click', () => window.print());
   }
 
   /**
@@ -132,11 +152,6 @@ export class Griddler extends CustomElementBase {
     }
   }
 
-  /** Exports the grid in a condensed format. */
-  export() {
-    console.log(XGrid.ToDense(this._grid));
-  }
-
   /**
    * Redraws the entire grid in accordance with the current state.
    */
@@ -149,42 +164,40 @@ export class Griddler extends CustomElementBase {
     this.totalWidth = grid_w + labels_w;
     this.totalHeight = grid_h + labels_h;
     const client_w = this.totalWidth / Griddler.RESOLUTION;
-    this.root.querySelector('.root').setAttribute('style', `width: ${client_w}px`);
+    this.root.querySelector('.grid-zone').setAttribute('style', `width: ${client_w}px`);
 
-    const gridContext = this._gridCanvas.getContext('2d');
-    this.clearContext(gridContext);
-    gridContext.imageSmoothingEnabled = false;
-    gridContext.beginPath();
+    this.clearContext(this._gridContext);
+    this._gridContext.fillStyle = '#fff';
+    this._gridContext.fillRect(0, 0, this.totalWidth, this.totalHeight);
+    this._gridContext.beginPath();
     for (let c = 0; c <= this.totalColumns; c++) {
-      gridContext.moveTo(c * this._size + Griddler.PIXEL_ADJUST, 0);
-      gridContext.lineTo(c * this._size + Griddler.PIXEL_ADJUST, grid_h);
+      this._gridContext.moveTo(c * this._size + Griddler.PIXEL_ADJUST, 0);
+      this._gridContext.lineTo(c * this._size + Griddler.PIXEL_ADJUST, grid_h);
     }
     for (let r = 0; r <= this.totalRows; r++) {
-      gridContext.moveTo(0, r * this._size + Griddler.PIXEL_ADJUST);
-      gridContext.lineTo(grid_w, r * this._size + Griddler.PIXEL_ADJUST);
+      this._gridContext.moveTo(0, r * this._size + Griddler.PIXEL_ADJUST);
+      this._gridContext.lineTo(grid_w, r * this._size + Griddler.PIXEL_ADJUST);
     }
-    gridContext.strokeStyle = Griddler.MINOR_COL;
-    gridContext.lineWidth = Griddler.RESOLUTION;
-    gridContext.stroke();    
-    gridContext.closePath();
+    this._gridContext.strokeStyle = Griddler.MINOR_COL;
+    this._gridContext.lineWidth = Griddler.RESOLUTION;
+    this._gridContext.stroke();    
+    this._gridContext.closePath();
 
-    gridContext.beginPath();
+    this._gridContext.beginPath();
     for (let c = 0; c <= this.totalColumns; c += Griddler.XY_INTERVAL) {
-      gridContext.moveTo(c * this._size + Griddler.PIXEL_ADJUST, 0);
-      gridContext.lineTo(c * this._size + Griddler.PIXEL_ADJUST, grid_h);
+      this._gridContext.moveTo(c * this._size + Griddler.PIXEL_ADJUST, 0);
+      this._gridContext.lineTo(c * this._size + Griddler.PIXEL_ADJUST, grid_h);
     }
     for (let r = 0; r <= this.totalRows; r += Griddler.XY_INTERVAL) {
-      gridContext.moveTo(0, r * this._size + Griddler.PIXEL_ADJUST);
-      gridContext.lineTo(grid_w, r * this._size + Griddler.PIXEL_ADJUST);
+      this._gridContext.moveTo(0, r * this._size + Griddler.PIXEL_ADJUST);
+      this._gridContext.lineTo(grid_w, r * this._size + Griddler.PIXEL_ADJUST);
     }
-    gridContext.strokeStyle = Griddler.MAJOR_COL;
-    gridContext.lineWidth = Griddler.RESOLUTION;
-    gridContext.stroke();    
-    gridContext.closePath();
-
-    this._hiContext.fillStyle = Griddler.HIGHLIGHT;
+    this._gridContext.strokeStyle = Griddler.MAJOR_COL;
+    this._gridContext.lineWidth = Griddler.RESOLUTION;
+    this._gridContext.stroke();    
+    this._gridContext.closePath();
     
-    this.populate(gridContext);
+    this.populate();
   }
 
   connectedCallback() {
@@ -222,6 +235,13 @@ export class Griddler extends CustomElementBase {
     }
   }
 
+  private static Download(dataUrl: string, filename: string) {
+    const tempLink = document.createElement('a');
+    tempLink.href = dataUrl;
+    tempLink.download = filename;
+    tempLink.click();
+  }
+
   private static Round(val: string|number, def: number, to: number, min: number, max: number) {
     val = parseInt(`${val}`)
     const rnd = to * Math.round((isNaN(val) ? def : val) / to);
@@ -239,80 +259,83 @@ export class Griddler extends CustomElementBase {
       const celRef = this._grid.rows[ri].cells;
       if (celRef[ci] !== state) {
         celRef[ci] = state;
-        point.ctx.beginPath();
-        this.setCell(point.ctx, ci, ri, false);
+        this._gridContext.beginPath();
+        this.setCell(ci, ri, false);
         switch (state) {
-          case 1: this.setCell(point.ctx, ci, ri, true); break;
-          case 2: this.markCell(point.ctx, ci, ri); break;
+          case 1: this.setCell(ci, ri, true); break;
+          case 2: this.markCell(ci, ri); break;
         }
-        point.ctx.fill();
+
+        this._gridContext.fill();
       }
     }
   }
 
-  private markCell(ctx: CanvasRenderingContext2D, ci: number, ri: number) {
+  private markCell(ci: number, ri: number) {
     const x0 = ci * this._size + Griddler.PIXEL_ADJUST + (this._size / 2);
     const y0 = ri * this._size + Griddler.PIXEL_ADJUST + (this._size / 2);
-    ctx.moveTo(x0, y0);
-    ctx.arc(x0, y0, this._size / 8, 0, 2 * Math.PI);
+    this._gridContext.moveTo(x0, y0);
+    this._gridContext.arc(x0, y0, this._size / 8, 0, 2 * Math.PI);
   }
 
-  private setCell(ctx: CanvasRenderingContext2D, ci: number, ri: number, doFill: boolean) {
+  private setCell(ci: number, ri: number, doFill: boolean) {
     const buffer = 2 * Griddler.PIXEL_ADJUST;
     const method = doFill ? 'rect' : 'clearRect';
-    ctx[method](
+    this._gridContext[method](
       ci * this._size + buffer,
       ri * this._size + buffer,
       this._size - buffer,
       this._size - buffer);
   }
 
-  private populate(gridContext: CanvasRenderingContext2D) {
-
-    // cell states
-    gridContext.beginPath();
-    this._grid.rows
-      .map((row, idx) => ({ cells: row.cells, idx }))
-      .forEach(row => (row.cells || [])
-        .map((state, idx) => ({ state, idx }))
-        .forEach(cell => {
-          switch (cell.state) {
-            case 1: this.setCell(gridContext, cell.idx, row.idx, true); break;
-            case 2: this.markCell(gridContext, cell.idx, row.idx); break;
-          }
-        })
-      );
-    gridContext.fill();
+  private populate() {
 
     // labels
     const grid_w = this.totalColumns * this._size + Griddler.PIXEL_ADJUST;
     const grid_h = this.totalRows * this._size + Griddler.PIXEL_ADJUST;
     const font_size = this._size * .55;
-    gridContext.font = `${font_size}px Times New Roman`;
+    this._gridContext.font = `${font_size}px Times New Roman`;
+    this._gridContext.fillStyle = Griddler.LABEL_COL;
 
-    gridContext.textAlign = 'left';
+    this._gridContext.textAlign = 'left';
     this._grid.rows
       .map((row, idx) => ({ labels: row.labels, idx }))
       .filter(set => set.labels && set.labels.length > 0)
       .forEach(set => {
         const x = grid_w + (font_size / 2);
         const y = set.idx * this._size + (this._size / 2) + (font_size / 2);
-        gridContext.fillText(set.labels.join(' . '), x, y);
+        this._gridContext.fillText(set.labels.join(' . '), x, y);
       });
 
-    gridContext.textAlign = 'center';
+    this._gridContext.textAlign = 'center';
     this._grid.columns
       .map((col, idx) => ({ labels: col.labels, idx }))
       .filter(set => set.labels && set.labels.length > 0)
       .forEach(set => {
         const x = set.idx * this._size + (this._size / 2) + 2;
         set.labels.forEach((label, idx) => {
-          gridContext.fillText(label + '', x, grid_h + ((font_size * 1.2) * (idx + 1.2)));
+          this._gridContext.fillText(label + '', x, grid_h + ((font_size * 1.2) * (idx + 1.2)));
         });
       });
+
+    // cell states
+    this._gridContext.beginPath();
+    this._grid.rows
+      .map((row, idx) => ({ cells: row.cells, idx }))
+      .forEach(row => (row.cells || [])
+        .map((state, idx) => ({ state, idx }))
+        .forEach(cell => {
+          switch (cell.state) {
+            case 1: this.setCell(cell.idx, row.idx, true); break;
+            case 2: this.markCell(cell.idx, row.idx); break;
+          }
+        })
+      );
+    this._gridContext.fillStyle = Griddler.CELL_COL;
+    this._gridContext.fill();
   }
 
-  private clearContext(context: CanvasRect) {
+  private clearContext(context: CanvasRenderingContext2D) {
     context.clearRect(0, 0, this.totalWidth, this.totalHeight);
   }
 
@@ -329,22 +352,18 @@ export class Griddler extends CustomElementBase {
       x0: colIdx * this._size + Griddler.PIXEL_ADJUST,
       y0: rowIdx * this._size + Griddler.PIXEL_ADJUST,
       which: locator.which,
-      state: this.getState(gridDims),
-      ctx: this._gridCanvas.getContext('2d'),
+      state: this.getState(gridDims)
     };
   }
 
   private highlight(coords: GridContextPoint) {
     this.clearContext(this._hiContext);
+    this._hiContext.fillStyle = Griddler.HILITE;
     if (coords.x) this._hiContext.fillRect(coords.x0, 0, this._size, this.totalHeight);
     if (coords.y) this._hiContext.fillRect(0, coords.y0, this.totalWidth, this._size);
     if (coords.x && coords.y) {
       this._hiContext.clearRect(coords.x0, coords.y0, this._size, this._size);
     }
-  }
-
-  private log(coords: GridContextPoint) {
-    console.log(coords);
   }
 }
 
@@ -358,5 +377,4 @@ interface GridContextPoint extends Point {
   y0: number;
   which: number;
   state: 0 | 1 | 2;
-  ctx: CanvasRenderingContext2D;
 }
